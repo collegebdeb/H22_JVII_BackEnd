b840f1003e3f3ea4e8caa3ee4efea398de78fff2
Justin Lepitre <lepitrejustin@gmail.com>
2022-05-04 16:14:06 -0400

    Character controller 2d and sprite animator complete

--- Assets/MinigameAssets/CharacterControllerMinigame.cs
+++ Assets/MinigameAssets/CharacterControllerMinigame.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using devziie.Inputs;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class CharacterControllerMinigame : MonoBehaviour
 {
@@ -14,8 +15,8 @@ public class CharacterControllerMinigame : MonoBehaviour
     private Vector2 _moveInput;
     float horizontalMove = 0f;
     bool jump = false;
-    public Transform GroundCheck;
-    public bool TouchingGround;
+    [FormerlySerializedAs("GroundCheck")] public Transform groundCheck;
+    [FormerlySerializedAs("TouchingGround")] public bool touchingGround;
     bool crouch = false;
     public Rigidbody2D _rbody;
     public LayerMask groundLayer2D;
@@ -24,9 +25,9 @@ public class CharacterControllerMinigame : MonoBehaviour
     public Sprite jumpSprite;
     public Sprite idleSprite;
     public Sprite[] moveSprite;
-    public float framesPerSecond;
+    public float delayAnimation;
     public float timer;
-    private int currentFrame;
+    public int currentFrame;
 
     // Update is called once per frame
     void Update()
@@ -85,29 +86,47 @@ public class CharacterControllerMinigame : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (timer <= framesPerSecond)
-        {
-            timer -= framesPerSecond;
-            currentFrame = (currentFrame + 1) % moveSprite.Length;
-            gameObject.GetComponent<SpriteRenderer>().sprite = moveSprite[currentFrame];
+        timer += Time.deltaTime;
 
+        if (timer >= delayAnimation)
+        {
+            
+            if (currentFrame == 0)
+            {
+                currentFrame = 1;
+            }
+            else
+            {
+                currentFrame = 0;
+            }
+            timer = 0f;
+                
         }
-        TouchingGround = Physics2D.OverlapCircle(GroundCheck.position, 0.01f, groundLayer2D);
+
+        if (touchingGround == true)
+        {
+            spriteRenderer.sprite = moveSprite[currentFrame];
+        }
+
+        touchingGround = Physics2D.OverlapCircle(groundCheck.position, 0.01f, groundLayer2D);
         // Move our character
         //controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         //jump = false;
 
         _rbody.velocity = new Vector2(_moveInput.x * runSpeed, _rbody.velocity.y);
-        if (jump == true && TouchingGround == true)
+        if (jump == true && touchingGround == true)
         {
             _rbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
-
+        if (touchingGround == false)
+        {
+            spriteRenderer.sprite = jumpSprite;
+        }
 
         if (_moveInput == Vector2.zero)
         {
             spriteRenderer.sprite = idleSprite;
-            if (TouchingGround == false)
+            if (touchingGround == false)
             {
                 spriteRenderer.sprite = jumpSprite;
             }
