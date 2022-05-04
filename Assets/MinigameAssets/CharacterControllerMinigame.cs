ab7f3f8037d5324fcdb542c988de6e053147b703
Justin Lepitre <lepitrejustin@gmail.com>
2022-05-04 13:32:42 -0400

    2D minigame character cobtroller done + import scene

--- Assets/MinigameAssets/CharacterControllerMinigame.cs
+++ Assets/MinigameAssets/CharacterControllerMinigame.cs
@@ -14,8 +14,12 @@ public class CharacterControllerMinigame : MonoBehaviour
     private Vector2 _moveInput;
     float horizontalMove = 0f;
     bool jump = false;
+    public Transform GroundCheck;
+    public bool TouchingGround;
     bool crouch = false;
     public Rigidbody2D _rbody;
+    public LayerMask groundLayer2D;
+    public float jumpForce;
 
     // Update is called once per frame
     void Update()
@@ -35,6 +39,7 @@ public class CharacterControllerMinigame : MonoBehaviour
     {
         InputManager.Controls.Player2D.Jump.performed += OnJump;
         InputManager.Controls.Player2D.Move.performed += OnMove;
+        InputManager.Controls.Player2D.Move.canceled += OnStop;
     }
 	
     private void OnMove(InputAction.CallbackContext context)
@@ -48,16 +53,22 @@ public class CharacterControllerMinigame : MonoBehaviour
         
     }
 
+    private void OnStop(InputAction.CallbackContext context)
+    {
+        _moveInput = Vector2.zero;
+    }
+
     void FixedUpdate()
     {
+        TouchingGround = Physics2D.OverlapCircle(GroundCheck.position, 0.15f, groundLayer2D);
         // Move our character
         //controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         //jump = false;
 
         _rbody.velocity = new Vector2(_moveInput.x * runSpeed, _rbody.velocity.y);
-        if (jump == true)
+        if (jump == true && TouchingGround == true)
         {
-            _rbody.AddForce(new Vector2(0, 15), ForceMode2D.Impulse);
+            _rbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
 
         jump = false;
