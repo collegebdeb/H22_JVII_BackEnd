e1feb0a492f5c24b8440f5248db1a1186b275bc4
Justin Lepitre <lepitrejustin@gmail.com>
2022-05-18 15:21:26 -0400

    3 levels

--- Assets/ControlLock.cs
+++ Assets/ControlLock.cs
@@ -14,7 +14,7 @@ public class ControlLock : MonoBehaviour
     void Start()
     {
         StartCoroutine(OrderAction());
-        InputManager.Controls.Player.ToggleBackEnd.Disable();
+        // InputManager.Controls.Player.ToggleBackEnd.Disable();
         if(lockBeginControl) InputManager.Controls.Player.Disable();
         
 
